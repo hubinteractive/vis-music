be0ab288ae9fd44176e014d4ec31c5d212bc1105
Igor Nikolovski <nikolovski_igor@icloud.com>
2024-05-05 13:02:08 +0200

    v.05.05.24-1

--- vis_breeze/urls.py
+++ vis_breeze/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import index, about, contact
+from .views import index, about, contact, lana_janjanin, jazz_orchestra, kafka, vlatko, dilatum
 
 
 app_name = 'vis_breeze'
@@ -9,6 +9,11 @@ urlpatterns = [
     path('', index, name='index'),
     path('about/', about, name='about_us'),
     path('contact/', contact, name='contact_us'),
+    path('lana_janjanin/', lana_janjanin, name='lana_janjanin'),
+    path('jazz_orchestra/', jazz_orchestra, name='jazz_orchestra'),
+    path('kafka/', kafka, name='kafka'),
+    path('vlatko/', vlatko, name='vlatko'),
+    path('dilatum/', dilatum, name='dilatum'),
     # path('login_cnd', loginCnd, name='login_cnd'),
     # path('about_cnd', about, name='about_cnd'),
 
--- vis_breeze/views.py
+++ vis_breeze/views.py
@@ -22,3 +22,41 @@ def contact(request):
 
     }
     return render(request, 'contact.html',  context)
+
+
+
+def lana_janjanin(request):
+    context = {
+
+    }
+    return render(request, 'lana_janjanin.html',  context)
+
+
+def jazz_orchestra(request):
+    context = {
+
+    }
+    return render(request, 'jazz_orchestra.html',  context)
+
+
+def kafka(request):
+    context = {
+
+    }
+    return render(request, 'kafka.html',  context)
+
+
+
+def vlatko(request):
+    context = {
+
+    }
+    return render(request, 'vlatko.html',  context)
+
+
+
+def dilatum(request):
+    context = {
+
+    }
+    return render(request, 'dilatum.html',  context)
